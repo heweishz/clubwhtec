384cf0ed60dfb46901f6ab68a345202f31f239bd
heweishz <heweishz@163.com>
2019-09-11 05:16:19 +0800

    Update card.php

--- card.php
+++ card.php
@@ -1,16 +1,11 @@
-
 <?php
     $cid=$_GET["cid"];
-    $mysqli=new mysqli("cdb-adcxju0z.gz.tencentcdb.com:10059","root","53444r00m","card") or die('connect sql die');
+    $mysqli=new mysqli("cdb-adcxju0z.gz.tencentcdb.com:10059","root","53444r00m","card");
     
     $result=$mysqli->query("select id,title from card where cid={$cid}");
-    //print_r($result);
-    //$row = mysqli_fetch_assoc($result);
-    //print_r($row);
-    
-    while ($row=$result->fetch_assoc()) {
+    while ($row=$result->_assoc()) {
         echo '<ul>';
         echo '<li><a href="con.php?id='.$row["id"].'">'.$row["title"].'</a></li>';
         echo '</ul>';
     }
-?>
+    ?>
